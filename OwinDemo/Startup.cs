d9a37695d9a0df3b1dc9f2ff87f83f7e7f34e56b
Aayush Vetwal <aayushvetwal@outlook.com>
2018-11-04 15:44:21 +0545

    Added a middleware

--- OwinDemo/Startup.cs
+++ OwinDemo/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Owin;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,16 @@ namespace OwinDemo
     {
         public static void Configuration(IAppBuilder app)
         {
+            //keep middlewares in desired order
+            //multiple middlewares constitute pipeline
+            //"next" is a reference to the next middleware in pipeline. so by executing "next"
+            //we are executing rest of the pipeline
+
+            app.Use(async (ctx, next) => {
+                Debug.WriteLine("Incoming Request: " + ctx.Request.Path);
+                await next();
+                Debug.WriteLine("Outgoing Request: " + ctx.Request.Path);
+            });
             app.Use(async (ctx, next) => {
                 await ctx.Response.WriteAsync("<html><head></head><body>Hello World</body></html>");
             });
