3bc332083dda1d230c186f48b75d7e450d5e399e
Aayush Vetwal <aayushvetwal@outlook.com>
2018-11-04 13:17:41 +0545

    Added html response

--- OwinDemo/Startup.cs
+++ OwinDemo/Startup.cs
@@ -11,7 +11,7 @@ namespace OwinDemo
         public static void Configuration(IAppBuilder app)
         {
             app.Use(async (ctx, next) => {
-                await ctx.Response.WriteAsync("Hello World");
+                await ctx.Response.WriteAsync("<html><head></head><body>Hello World</body></html>");
             });
             //ctx is IOwinContext; it is just a wrapper around "environment" variable
             //next is a delegate
