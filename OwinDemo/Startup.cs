0112d395ee85fc29736fffb48dadde4e62897237
Aayush Vetwal <aayushvetwal@outlook.com>
2018-11-04 13:05:35 +0545

    Add project files.

---
+++ OwinDemo/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OwinDemo
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            app.Use(async (ctx, next) => {
+                await ctx.Response.WriteAsync("Hello World");
+            });
+            //ctx is IOwinContext; it is just a wrapper around "environment" variable
+            //next is a delegate
+        }
+    }
+}
